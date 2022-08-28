136e2b577a0600bf7fc95eb38c87aef5ad18d100
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-28 13:17:42 +0530

    Update SharedWallet.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -9,6 +9,9 @@ contract SharedWallet is Allowance {
     event MoneySent(address indexed _beneficiary, uint _amount);
     event MoneyReceived(address indexed _from, uint _amount);
 
+    function receiveMoney() public payable {
+
+    }
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "Contract doesn't own enough money");
         if(!isOwner()) {
